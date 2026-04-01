e0173bb606f411a78e2af3574a0b7c669bd18276
mirikim404 <mirikim8989@gmail.com>
2026-04-01 11:35:24 +0900

    first commit

---
+++ BaekJoon/src/BOJ10926.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class BOJ10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.nextLine();
+		
+		System.out.print(id + "??!");
+
+	}
+
+}
